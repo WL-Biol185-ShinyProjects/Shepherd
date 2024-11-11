9845ad3f1e85b419f7129b1c8203caf60910b7df
Lizzy Nguyen <nguyenlh25@mail.wlu.edu>
2024-11-11 15:51:10 -0500

    Attempt to push ui.r and shiny app Markdown

--- ui.r
+++ ui.r
@@ -2,6 +2,7 @@
 
 library(shiny)
 library(leaflet)
+library(bslib)
 
 #Define UI for application that draws a histogram
 fluidPage(
@@ -12,4 +13,24 @@ fluidPage(
   #Add a button!
   p(actionButton("titleButton", "Click Here", icon = NULL, width = NULL)),
   
+  navset_card_underline(
+    
+    nav_panel("Plot", plotOutput("plot")),
+    
+    nav_panel("Summary", tableOutput("summary")),
+    
+    nav_panel("Data", DT::dataTableOutput("data")),
+    
+    nav_panel(
+      "Reference",
+      markdown(
+        glue::glue(
+          "These data were obtained from [IMDB](http://www.imdb.com/) and [Rotten Tomatoes](https://www.rottentomatoes.com/).
+  
+        The data represent {nrow(movies)} randomly sampled movies released between 1972 to 2014 in the United States.
+        "
+        )
+      )
+    )
+  )
 )
