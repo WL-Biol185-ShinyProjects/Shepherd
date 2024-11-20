6a1c36415d1d12f537b2ca96759f422e8ceba44b
Lizzy Nguyen <nguyenlh25@mail.wlu.edu>
2024-11-20 16:14:37 -0500

    lots of changes, almost getting it to work

--- ui.r
+++ ui.r
@@ -2,53 +2,69 @@
 
 
 library(shiny)
+library(tidyverse)
+library(dplyr)
 library(leaflet)
+
+#Loading all relevant data sets
+obese_overweight_adults <- read_csv("obese_overweight_adults.csv")
+GDP_tidy <- read_csv("GDP_tidy.csv")
+Gini_Inequality_Index_tidy <- read_csv("Gini_Inequality_Index_tidy.csv")
+happiness_index_tidy <- read_csv("happiness_index_tidy.csv")
+
 
 # Create the UI
 shinyUI(
-  navbarPage(
-    "Global Trends",
-
-    # First Panel
-    tabPanel(
-      "Choropleth Map",
-      sidebarLayout(
-        sidebarPanel(
-          # Year slider
-          sliderInput(
-            inputId = "year",
-            label = "Year:",
-            min = 1975,
-            max = 2016,
-            value = 1975,
-            step = 1
-          ),
-          
-          # Global factors radio buttons
-          radioButtons(
-            inputId = "radio", 
-            label = h3("Global Factors"),
-            choices = list(
-              "Adult Obesity" = "obesity", 
-              "Gross GDP" = "gdp", 
-              "Gini Inequality Index" = "gini", 
-              "Happiness Index" = "happiness"
-            ),
-            selected = "obesity" # Default selected value
-          )
-        ),
-        
+  navbarPage("Happy Meals: Global Factors Related to Obesity",
+             
+             
+             #CREATING FIRST PANEL: LONGITUDINAL CHLOROPLETHS
+             
+             tabPanel(
+               "Longitudinal Chloropleths",
+               sidebarLayout(
+                 sidebarPanel(
+                   
+                   
+                   #SLIDER
+                   sliderInput( "year",
+                                "Year:",
+                                min = min(obese_overweight_adults$Year),
+                                max = max(obese_overweight_adults$Year),
+                                value = c(1975)
+                   ),
+                   
+                   
+                   # Select which global factor - dropdown menu
+                   selectInput("GlobalFactor",
+                               label = "Choose a Global Factor",
+                               choices = list("Adult Obesity" = obese_overweight_adults[obese_overweight_adults$Year == input$year,
+                                                                                        c("Entity", "percentBMI30", input$var)],
+                                  
+                                              "Gross GDP" = GDP_tidy[GDP_tidy$year == input$year,
+                                                                     c("country name", "gdp", input$var)], 
+                                              
+                                              "Gini Inequality Index" = Gini_Inequality_Index_tidy[Gini_Inequality_Index_tidy$year == input$year,
+                                                                                                   c("country name", "gini inequality index", input$var)], 
+                                              
+                                              
+                                              "Happiness Index" = happiness_index_tidy[happiness_index_tidy$year == input$year,
+                                                                                       c("country", "positive affect", input$var)], 
+                               selected = "Gross GDP")
+                   
+                 ),
         # Main panel for the Leaflet map
         mainPanel(
           leafletOutput(outputId = "map", height = "600px")
         )
       )
-    )
+    ), 
+    
+    #SECOND PANEL: CORRELATION MATRIX
+    tabPanel("Correlation Matrix"), 
+    tabPanel("Fast Food Map Mania") 
+    
   )
 )
-             #SECOND PANEL: CORRELATION MATRIX
-             tabPanel("Correlation Matrix")
-             tabPanel("Fast Food Map Mania")
-  
-
+)
 
