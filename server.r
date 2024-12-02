9880c51c2dd7c29d5f4e8443c82b6b43f0579ab0
Lizzy Nguyen <nguyenlh25@mail.wlu.edu>
2024-12-02 15:26:36 -0500

    attempt to merge widget code and chloropleth code; resource temporarily unavailable error

--- server.r
+++ server.r
@@ -1,8 +1,8 @@
+
 #Server.R for Shepherd Project
 
 library(shiny)
 library(leaflet)
-
 
 #Loading all datasets
 obese_overweight_adults <- read_csv("obese_overweight_adults.csv")
@@ -12,6 +12,26 @@ happiness_index_tidy <- read_csv("happiness_index_tidy.csv")
 
 countries.geo.json <- geojson_read("countries.geo.json")
 
+#Update selectInput for variables based on selected GlobalFactor
+
+output$variableSelect <- renderUI({
+  data <- switch(input$GlobalFactor,
+                 "Adult Obesity" = obese_overweight_adults[obese_overweight_adults$year == input$year,
+                                                           c("country", "percentBMI30", input$var)],
+                 
+                 "Gross GDP" = GDP_tidy[GDP_tidy$year == input$year,
+                                        c("country name", "gdp", input$var)], 
+                 
+                 "Gini Inequality Index" = Gini_Inequality_Index_tidy[Gini_Inequality_Index_tidy$year == input$year,
+                                                                      c("country name", "gini inequality index", input$var)], 
+                 
+                 
+                 "Happiness Index" = happiness_index_tidy[happiness_index_tidy$year == input$year,
+                                                          c("country", "positive affect", input$var)], 
+                 
+                 )
+  
+})
 
 #Load geographic data 
 
@@ -19,5 +39,5 @@ geo <- geojson_read("countries.geo.json", what = "sp")
 
 #Load  based on selected factor 
 load_factor_data <- function(GlobalFactor) {
-  }
-    
+}
+
--- ui.r
+++ ui.r
@@ -4,7 +4,7 @@ library(shiny)
 library(tidyverse)
 library(dplyr)
 library(leaflet)
-
+library(geojsonio)
 
 #Loading all relevant data sets
 obese_overweight_adults <- read_csv("obese_overweight_adults.csv")
@@ -17,51 +17,56 @@ happiness_index_tidy <- read_csv("happiness_index_tidy.csv")
 
 shinyUI(
   navbarPage("Happy Meals: Global Factors Related to Obesity",
-             
              #FIRST PANEL: LONGITUDINAL CHLOROPLETHS 
              tabPanel(
                "Longitudinal Chloropleths",
+               
                sidebarLayout(
-                 sidebarPanel(
-                   
-                   #SLIDER
-                   sliderInput( "year",
-                                "Year:",
-                                min = min(obese_overweight_adults$year),
-                                max = max(obese_overweight_adults$year),
-                                value = c(1975)
-                   ),
-                   
-                   
-                   # Select which global factor - dropdown menu
-                   selectInput("GlobalFactor",
-                               label = "Choose a Global Factor",
-                               choices = list("Adult Obesity" = obese_overweight_adults[obese_overweight_adults$year == input$year,
-                                                                                        c("country", "percentBMI30", input$var)],
-                                  
-                                              "Gross GDP" = GDP_tidy[GDP_tidy$year == input$year,
-                                                                     c("country name", "gdp", input$var)], 
-                                              
-                                              "Gini Inequality Index" = Gini_Inequality_Index_tidy[Gini_Inequality_Index_tidy$year == input$year,
-                                                                                                   c("country name", "gini inequality index", input$var)], 
-                                              
-                                              
-                                              "Happiness Index" = happiness_index_tidy[happiness_index_tidy$year == input$year,
-                                                                                       c("country", "positive affect", input$var)], 
-                               selected = "Gross GDP")
-                   
-                 ),
-        # Main panel for the Leaflet map
-        mainPanel(
-          leafletOutput(outputId = "map", height = "600px")
-        )
-      )
-    ), 
-    
-    #SECOND PANEL: CORRELATION MATRIX
-    tabPanel("Correlation Matrix"), 
-    tabPanel("Fast Food Map Mania") 
-    
+                 
+                 
+                     sidebarPanel(
+                       
+                       #SLIDER
+                       sliderInput( "year",
+                                    "Year:",
+                                    min = min(obese_overweight_adults$year),
+                                    max = max(obese_overweight_adults$year),
+                                    value = c(1975)
+                       ),
+                       
+                       
+                       # Select which global factor - dropdown menu
+                       #DEBUG NOTE: input isn't an object, R is trying to read it as one
+                       selectInput("GlobalFactor",
+                                   label = "Choose a Global Factor",
+                                   choices = list("Adult Obesity" = "obese_overweight_adults",
+                                                 
+                                                  "Gross GDP" = "GDP_tidy",
+                                                  
+                                                  "Gini Inequality Index" = "Gini_Inequality_Index_tidy",
+                                                  
+                                                  "Happiness Index" = "happiness_index_tidy",
+              
+                                                  selected = "Gross GDP"),
+                  
+          
+                )             
+              ),
+              
+              # Main panel for the Leaflet map
+                      mainPanel(
+                        #leafletOutput(outputId = "map", height = "600px")
+                   )
+         ), 
+         
+               #SECOND PANEL: CORRELATION MATRIX
+               tabPanel("Correlation Matrix"), 
+               
+               #THIRD PANEL: Fast Food Map Mania
+               tabPanel("Fast Food Map Mania") 
+               
+             )
   )
 )
-)
+
+
